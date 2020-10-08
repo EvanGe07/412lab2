13573ccfce55160d4681fa4a04ee00b1752ef74c
Zehou Ge <861536051@qq.com>
2020-10-08 12:25:55 +0800

    modified print

--- 412alloc.py
+++ 412alloc.py
@@ -11,13 +11,13 @@ def main():
     try:
         opts, args = getopt.getopt(sys.argv[1:], 'x:h')
     except getopt.GetoptError as err:
-        print(sys.stderr, err)
+        print(err, file=sys.stderr)
         usage()
         sys.exit(2)
 
     if sys.argv[1].isdigit():
         if int(sys.argv[1]) > 64 or int(sys.argv[1]) < 3:
-            print("ERROR: k must be an integer between 3 and 64.")
+            print("ERROR: k must be an integer between 3 and 64.", file=sys.stderr)
         else:
             allocate(sys.argv[2], int(sys.argv[1]))
     else:
@@ -48,16 +48,6 @@ def allocate(filename, k):
             allocator = AllocatorWithSpill.AllocatorWithSpill(renamer, k)
             allocator.allocate_with_spill()
             allocator.print_allocated_block()
-            # decide whether to reserve the last pr for spill and restore
-            # by evaluating MAXLIVE > k
-            # if renamer.max_live > k:
-            #     allocator = AllocatorWithSpill.AllocatorWithSpill(renamer, k)
-            #     allocator.allocate_with_spill()
-            #     allocator.print_allocated_block()
-            # else:
-            #     allocator = AllocatorWithoutSpill.AllocatorWithoutSpill(renamer, k)
-            #     allocator.allocate_without_spill()
-            #     allocator.print_allocated_block()
     else:
         print('\nParser found %i syntax errors in %i lines of input.' \
                              % (parser.num_errors, parser.scanner.lineNo - 1))
@@ -82,15 +72,15 @@ def rename(filename):
 
 
 def usage():
-    print("COMP 412, Fall 2020 Local Register Allocation (Lab 2)")
-    print("Command Syntax:")
-    print("      412alloc k filename [-h] [-x]")
-    print("\nRequired arguments:")
-    print("       k        specifies the number of register available")
-    print("       filename the pathname (absolute or relative) to the input file")
-    print("\nOptional flags:")
-    print("       -h        prints this message")
-    print("       -x        peforms register renaming")
+    print("COMP 412, Fall 2020 Local Register Allocation (Lab 2)", file=sys.stderr)
+    print("Command Syntax:", file=sys.stderr)
+    print("      412alloc k filename [-h] [-x]", file=sys.stderr)
+    print("\nRequired arguments:", file=sys.stderr)
+    print("       k        specifies the number of register available", file=sys.stderr)
+    print("       filename the pathname (absolute or relative) to the input file", file=sys.stderr)
+    print("\nOptional flags:", file=sys.stderr)
+    print("       -h        prints this message", file=sys.stderr)
+    print("       -x        peforms register renaming", file=sys.stderr)
 
 
 if __name__ == "__main__":
